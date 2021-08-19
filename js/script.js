580f7407109cdf4a63ff2ff1335e484703549765
William Rodrigues <williamrodrigues2442@gmail.com>
2021-08-19 06:01:58 -0300

    Add audio on introduction page, and a way to user to toggle the audio

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
  * General helper functions that are used in some pages.
  */
 
+const audio = new Audio();
+
 /**
  * Redirects the user to a page, depending on the response by the user.
  * 
@@ -49,15 +51,36 @@ function redirect(page) {
  * Plays an audio on the page.
  * 
  * @param {string} audioPath A relative path to the audio path, related to the HTML page that's calling the function.
- * @param {number} volume A number between 0 and 1, where 0 means muted and 1 completly audible. 
+ * @param {number} volume A number between 0 and 1, where 0 means muted and 1 completly audible (default: 0.5).
+ * @param {boolean} loop A boolean that specifies if the audio should restart when it ends (default: false).
  */
-function playAudio(audioPath, volume=0.5) {
-  const audio = new Audio(audioPath);
+function playAudio(audioPath, volume=0.5, loop=false) {
+  audio.src = audioPath;
 
   audio.addEventListener('canplaythrough', () => {
     audio.volume = volume;
+    audio.loop = loop;
     audio.play();
   });
+}
+
+/** Pause and unpause a audio. */
+function toggleAudio() {
+  audio.paused ? audio.play() : audio.pause();
+}
+
+/** Toggles the audio button.  */
+function toggleAudioButtonIcon() {
+  const button = document.getElementById('btn-audio');
+  const audioIsPlaying = button.getAttribute('data-audio') === 'on';
+
+  if (audioIsPlaying) {
+    button.innerHTML = '<span class="fas fa-volume-mute"></span>';
+    button.setAttribute('data-audio', 'off');
+  } else {
+    button.innerHTML = '<span class="fas fa-volume-up"></span>';
+    button.setAttribute('data-audio', 'on');
+  }
 }
 
 /** Creates an fade in effect in the body content. */
